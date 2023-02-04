a4701f0a4dc587d7158241ac7439da5c8f7569e7
Advika Jayanti <advika.jayanti@gmail.com>
2023-02-04 13:04:41 -0500

    json conversion

--- postListings.php
+++ postListings.php
@@ -23,20 +23,70 @@
     //     }
     // }
 
-    if(isset($_REQUEST['submit'])){
-        $data='';
-        $filename= "data.json";
-        if(is_file($filename)){
-            $data=file_get_contents($filename);
-        }
-        $json_arr= json_decode($data,true);
-        $json_arr[]=array('code'=> $_REQUEST['code']'name'=> $_REQUEST['name']);
+    ​if(empty($_GET["firstname"]))
+{
+     $error = "<label class='text-danger'>Enter First Name</label>";
+}
+else if(empty($_GET["lastname"]))
+{
+     $error = "<label class='text-danger'>Enter Last Name</label>";
+}
+else if(empty($_GET["phone"]))
+{
+     $error = "<label class='text-danger'>Enter Phone Number</label>";
+}  else if(empty($_GET["email"]))
+{
+     $error = "<label class='text-danger'>Enter email</label>";
+}
 
-        file_put_contents($filename,json_encode($json_arr));
 
-        header("location: https://eringgao.github.io/lucy_listings/Post_listing.html")
-   
+// Add to JSON file:
 
-    }
+if(file_exists('file.json'))
+{
+     $final_data=fileWriteAppend();
+     if(file_put_contents('file.json', $final_data))
+     {
+          $message = "<label class='text-success'>Data added Success fully</p>";
+     }
+}
+else
+{
+     $final_data=fileCreateWrite();
+     if(file_put_contents('file.json', $final_data))
+     {
+          $message = "<label class='text-success'>File createed and  data added Success fully</p>";
+     }
+
+}
+function fileWriteAppend(){
+		$current_data = file_get_contents('file.json');
+		$array_data = json_decode($current_data, true);
+		$extra = array(
+			 'first_name'      =>     $_GET['firstname'],
+			 'last_name'       =>     $_GET["lastname"],
+			 'phone'           =>     $_GET["phone"],
+			 'email'           =>     $_GET["email"],
+		);
+		$array_data[] = $extra;
+		$final_data = json_encode($array_data);
+		return $final_data;
+}
+function fileCreateWrite(){
+		$file=fopen("file.json","w");
+		$array_data=array();
+		$extra = array(
+            'first_name'      =>     $_GET['firstname'],
+            'last_name'       =>     $_GET["lastname"],
+            'phone'           =>     $_GET["phone"],
+            'email'           =>     $_GET["email"],
+
+		);
+		$array_data[] = $extra;
+		$final_data = json_encode($array_data);
+		fclose($file);
+		return $final_data;
+}
+	 
     
 ?>
